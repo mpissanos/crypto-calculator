24a8c25e2d35c98ee884261f5368fd68ca315daf
Manuel Pissanos <mpissano@gmail.com>
2019-06-25 18:26:01 -0400

    added controller and HTTParty

---
+++ app/controllers/currencies_controller.rb
@@ -0,0 +1,15 @@
+class CurrenciesController < ApplicationController
+
+  def index
+  end
+
+  def search
+    @currencies = Currency.where('LOWER(name) LIKE ?' , "%#{params[:search].downcase}%")
+    render json: { currencies: @currencies }
+  end
+
+  def calculate
+
+  end
+
+end
